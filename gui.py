ee0643023284577021d1d5ee3099be625f765eee
Nicobossio <nicobossior42@gmail.com>
2023-05-29 08:25:27 -0500

    Cambios  y se anada la base de datos

--- gui.py
+++ gui.py
@@ -1,15 +1,21 @@
-#Se anade el pqt5 para integrarlo con el primer menu
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QApplication
 from funcmenu import DataEntryForm
 from data_display import DataDisplay
 from database import Database
+import sys
 
-class Application(QMainWindow):
+class Application(QApplication):
+    def __init__(self, argv):
+        super().__init__(argv)
+        self.main_window = MainWindow()
+        self.main_window.show()
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("CyberTracker Replica")
 
-        self.db = Database("data.db")  # Replace "data.db" with your desired database file name
+        self.db = Database("data.db")  # Reemplaza "data.db" con el nombre de archivo de base de datos deseado
 
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
@@ -21,7 +27,9 @@ class Application(QMainWindow):
         self.layout.addWidget(self.data_entry_form)
         self.layout.addWidget(self.data_display)
 
-        self.show()
-
-    def __del__(self):
+    def closeEvent(self, event):
         self.db.close()
+
+if __name__ == "__main__":
+    app = Application(sys.argv)
+    sys.exit(app.exec())
